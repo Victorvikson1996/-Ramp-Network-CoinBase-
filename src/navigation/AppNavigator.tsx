3dbcd6630f41c09fb1758b52578485a156c161dd
Victor Ezea <victorvikson@gmail.com>
2022-09-04 15:03:26 +0100

    initial commit after setup

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "react-native-screens/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const AppNavigator = () => {
+  return (
+    <View>
+      <Text>AppNavigator</Text>
+    </View>
+  );
+};
+
+export default AppNavigator;
